e79487f9af0920ba357bdd64f34f809fc33f0cca
jimmy nagles <jimmynagles@gmail.com>
2020-05-06 12:20:33 -0700

    card=panel

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,70 +1,89 @@
-import React from 'react';
-import NavBar from "./Components/Nav/NavBar"
-import {  Row, Col, Parallax } from 'react-materialize';
-import Container from './Components/Gird/Container'
-import Background from './Components/Gird/Background';
-import Vone from "./Videos/Vone.mp4"
-import Prallax from './Components/Gird/Prallax';
-import Card from './Components/Cards/Card';
-import PhotoOne from "./css/img/Photo (2).jpg"
-import PhotoTwo from "./css/img/Photo (5).jpg"
-import PhotoThree from "./css/img/Photo (9).jpg"
-import PhotoFour from "./css/img/Photo (11).jpg"
-
+import React from "react";
+import NavBar from "./Components/Nav/NavBar";
+import { Row, Col, Parallax } from "react-materialize";
+import Container from "./Components/Gird/Container";
+import Background from "./Components/Gird/Background";
+import Vone from "./Videos/Vone.mp4";
+import Prallax from "./Components/Gird/Prallax";
+import Card from "./Components/Cards/Card";
+import PhotoOne from "./css/img/Photo (2).jpg";
+import PhotoTwo from "./css/img/Photo (5).jpg";
+import PhotoThree from "./css/img/Photo (9).jpg";
+import PhotoFour from "./css/img/Photo (11).jpg";
+import CardPanel from "./Components/Cards/CardPanel";
 
 function App() {
   return (
     <div className="App">
-<Background ><h2>jjjjj</h2></Background>
+      <Background>
+        
+      </Background>
 
-<NavBar></NavBar>
-<Container className="red " >
-<Row>
-      <Col l={12}>
-    <h2 className="center white-text">Projects</h2>
+      <NavBar></NavBar>
 
+      <Container className=" valign-wrapper">
+        <CardPanel>
+          <div className="center">
+            <h1 className="white-text ">Welcome </h1>
+            <h1 className="white-text ">Full Stack Web Developer specialized in the MERN stack
+            </h1>
+          </div>
+        </CardPanel>
+      </Container>
 
-      </Col>
-    </Row>
+      <Container>
+        <Row>
+          <Col l={12}>
+            <h2 className="center white-text">Projects</h2>
+          </Col>
+        </Row>
+      </Container>
 
-  
-</Container>
-
-
-<Container>
-  <Row>
-  <Card 
-    image={PhotoFour}
-    title={<h8>food Search</h8>}
-    info={<h5>Searchs for recepies of food online using react , axios and express </h5>}
-    link={"google.com"}/>
-      <Card 
-    image={PhotoOne}
-    title={<h8>Online Rock papper Scissor game</h8>}
-    info={<h5>A game that was build using the mern stack  </h5>}
-    link={"google.com"}/>
-      <Card 
-    image={PhotoThree}
-    title={<h8>Tatt-it</h8>}
-    info={<h5>A collaboration project made using js axios nosql express and node.js</h5>}
-    link={"https://github.com/jontruong/Project2"}/>
-      <Card 
-    image={PhotoTwo}
-    title={<h8>members of the  parliment of zimbawe</h8>}
-    info={<h5>A collaboration  project made using the mern stack that hopes to inform people
-      about who represents them! more countries to come.
-    </h5>}
-    link={"https://github.com/lilaouadi/Project3"}/>
- 
-  </Row>
-    
-    </Container>
-    
-
-  
+      <Container>
+        <Row>
+          <Card
+            image={PhotoFour}
+            title={<h8>food Search</h8>}
+            info={
+              <h5>
+                Searchs for recepies of food online using react , axios and
+                express{" "}
+              </h5>
+            }
+            link={"google.com"}
+          />
+          <Card
+            image={PhotoOne}
+            title={<h8>Online Rock papper Scissor game</h8>}
+            info={<h5>A game that was build using the mern stack </h5>}
+            link={"google.com"}
+          />
+          <Card
+            image={PhotoThree}
+            title={<h8>Tatt-it</h8>}
+            info={
+              <h5>
+                A collaboration project made using js axios nosql express and
+                node.js
+              </h5>
+            }
+            link={"https://github.com/jontruong/Project2"}
+          />
+          <Card
+            image={PhotoTwo}
+            title={<h8>members of the parliment of zimbawe</h8>}
+            info={
+              <h5>
+                A collaboration project made using the mern stack that hopes to
+                inform people about who represents them! more countries to come.
+              </h5>
+            }
+            link={"https://github.com/lilaouadi/Project3"}
+          />
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
-
--- portfolio/src/Components/Gird/Background.js
+++ portfolio/src/Components/Gird/Background.js
@@ -1,43 +1,32 @@
 import React from "react";
 
-import Vone from '../../Videos/Vtwo.mp4'
-
+import Vone from "../../Videos/Vtwo.mp4";
 
 function Background(props) {
-    return (
-   
-      <div >
-            {props.nav}
+  return (
+    <div>
+      {props.nav}
 
-            <video autoPlay muted loop
-            
-            style={{
-                position:"absolute",
-                width:"100%",
-                left:"50%",  
-                top:"50%",
-                height:"auto",
-                objectFit:"cover",
-                transform:"translate(-50%,-50%)",
-                zIndex:"-1"
-            }}
-            
-            >
-                <source src={Vone}>
+      <video
+        autoPlay
+        muted
+        loop
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "49%",
+          height: "auto",
+          objectFit: "cover",
+          transform: "translate(-50%,-50%)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={Vone}></source>
+      </video>
+      <div>{props.content}</div>
+    </div>
+  );
+}
 
-            </source>
-            </video>
-            <div>
-
-            {props.content}
-
-            </div>
-
-      </div>
-   
-  
-    );
-  }
-  
-  export default Background;
-  
+export default Background;
